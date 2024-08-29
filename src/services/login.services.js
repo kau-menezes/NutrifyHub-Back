aa9046ebf82019d174fd9e236cc403b663a5052c
akauzinha47 <kau.vmenezes@gmail.com>
2024-08-29 20:37:13 -0300

    feat: error handling

---
+++ src/services/login.services.js
@@ -0,0 +1,19 @@
+import { where } from "sequelize";
+import User from "../models/user.model";
+import crypt from "bcryptjs"
+import AppError from "../AppError";
+
+export async function loginVerifyService(req, res) {
+
+    const user = await User.findOne({
+        where: { email: req.body.email }
+    })
+
+    if (!user) throw new AppError("Usuário não encontrado", 404)
+
+    if (!crypt.compareSync(req.body.password, user.password)) throw new AppError("Senha incorreta filhao", 401)
+    
+    res.status(200).json({
+        token: "deu boa fml"
+    })
+}
