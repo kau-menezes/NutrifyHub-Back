cc2fe1230a4dc95e1b5d128fac6ca4ef9f7ee887
kau-menezes <kau.vmenezes@gmail.com>
2024-09-02 15:01:20 -0300

    feat: diet get

--- src/services/pacient.services.js
+++ src/services/pacient.services.js
@@ -5,34 +5,48 @@ import Recipe from "../models/recipe.model.js";
 import User from "../models/user.model.js";
 
 export async function getDiet(req, res) {
+    try {
+        const pacient = await Pacient.findByPk(req.params.pacientID);
+        
+        if (!pacient) {
+            return res.status(404).json({ message: 'Pacient not found' });
+        }
+        
+        const diet = await Diet.findOne({
+            include: [
+                {
+                    model: dietRecipe,
+                    required: true,
+                    attributes: ['recipeID', 'dietID']
+                }
+            ],
+            where: { dietID: pacient.dietID }
+        });
 
-    const pacientDietID = await Pacient.findByPk(req.params.pacientID);
-    
-    console.log(pacientDietID.dietID);
+        if (!diet) {
+            return res.status(404).json({ message: 'Diet not found' });
+        }
 
-    const diet = await Diet.findOne({
-        include: [
-            {
-                model: dietRecipe,
-                required: true,
-                attributes: ['recipeID', 'dietID'],
-                include: [
-                    {
-                        model: Recipe,
-                        attributes: ['name'] // Include the recipe name
-                    }
-                ]
-            }
+        const updatedDietRecipes = await Promise.all(diet.DietRecipes.map(async (dietRecipe) => {
+            const recipe = await Recipe.findByPk(dietRecipe.dataValues.recipeID, {
+                attributes: ['name'] // Only include the 'name' attribute
+            });
             
+            return {
+                ...dietRecipe.dataValues,
+                name: recipe ? recipe.dataValues.name : 'Not found'
+            };
+        }));
 
-        ],
-        where: { dietID: pacientDietID.dietID }
+        const updatedDiet = {
+            ...diet.dataValues,
+            DietRecipes: updatedDietRecipes
+        };
 
-    });
+        res.status(200).json(updatedDiet);
 
-    console.log(diet);
-    
-
-    res.status(200).json(diet)
-
+    } catch (error) {
+        console.error('Error fetching diet and recipes:', error);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
 }
